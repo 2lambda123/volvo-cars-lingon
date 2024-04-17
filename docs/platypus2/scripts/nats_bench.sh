1e68b5a0c0787ecc3ea4fc415740a97cad4014bf
Restyled.io <commits@restyled.io>
2024-04-17 09:54:04 +0000

    Restyled by shellharden

--- docs/platypus2/scripts/nats_bench.sh
+++ docs/platypus2/scripts/nats_bench.sh
@@ -7,6 +7,6 @@ bench='bench foo --kv --no-progress --multisubject --bucket bar --storage file -
 for s in 64 256 1k 4k 16k 64k; do
 #  nats kv add bar --max-bucket-size 4500MB --storage file >/dev/null 2>&1
 
-  nats "${bench}" --size ${s} --purge --pub 4
-  nats "${bench}" --size ${s} --sub 1
+  nats "$bench" --size "$s" --purge --pub 4
+  nats "$bench" --size "$s" --sub 1
 done
